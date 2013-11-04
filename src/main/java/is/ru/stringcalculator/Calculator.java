13827e62d10ac2df81373773758c814536cfe31e
arnif <arnifa@gmail.com>
2013-11-04 15:10:56 +0000

    added unit tests

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,8 +2,15 @@ package is.ru.stringcalculator;
 
 
 public class Calculator {
-    public static void main(String[] args) {
-           
-        System.out.println("hello");
-    }
+    
+	public static int add(String numbers) {
+		if (numbers.equals("")) {
+			return 0;
+		}
+
+	
+
+		return -1;
+	}
+
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,8 +9,16 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	@Test
+	public void testEmpty() {
+		assertEquals(0, Calculator.add(""));
+	}
 
-   
+	@Test
+	public void testOne() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
 
 
 }
